d91ad948f07594abf45f93e1aec358cc17a1aaad
Custom Commands <rayktd@gmail.com>
2022-12-29 02:09:57 -0600

    !blank validation complete

--- server/utils/projectManagerFileSystem.js
+++ server/utils/projectManagerFileSystem.js
@@ -1,8 +1,6 @@
 const { v4: uuidv4, validate } = require('uuid');
 const fs = require('fs');
 
-let projectsList = [];
-
 /**
  * @description Checks if the project directory exists
  * @author NinjaSurge
@@ -12,8 +10,7 @@ const checkFS = (project_directory) => {
   try {
     fs.existsSync(project_directory);
   } catch (err) {
-    console.error(err);
-    throw new Error('No Projects Directory Detected');
+    throw new Error(`No Projects Directory Detected at ${project_directory}`);
   }
 };
 
@@ -24,21 +21,29 @@ const checkFS = (project_directory) => {
  */
 const listProjects = (project_directory) => {
   checkFS(project_directory);
+
   projectsList = [];
+
   const folders = fs.readdirSync(project_directory);
+
   for (i = 0; i < folders.length; i++) {
     try {
       if (!fs.existsSync(`${project_directory}/${folders[i]}/.pm.json`))
         continue;
+
       const data = fs.readFileSync(
         `${project_directory}/${folders[i]}/.pm.json`
       );
+
       const project = JSON.parse(data);
+
       projectsList.push(project);
     } catch (err) {
       throw new Error(`Error reading file from disk: ${err}`);
     }
   }
+
+  return projectsList;
 };
 
 /**
@@ -51,35 +56,28 @@ const getProjects = (project_directory) => {
   try {
     checkFS(project_directory);
 
-    listProjects(project_directory);
-
-    return projectsList;
+    return listProjects(project_directory);
   } catch (err) {
-    console.error(err);
-    throw new Error('There was a problem reading the Project Directory');
+    throw new Error(
+      'There was a problem reading projects from the project directory'
+    );
   }
 };
 
 /**
  * @description Returns the project config file
  * @author NinjaSurge
- * @param {*} project_directory - The directory containing all the projects
- * @param {*} id - The id of the project to be returned
- * @return {*}
+ * @param {String} project_directory - The directory containing all the projects
+ * @param {String} id - The id of the project to be returned
+ * @return {*} - returns the project object with a matching id
  */
 const getProject = (project_directory, id) => {
+  // Checks
   checkFS(project_directory);
-  listProjects(project_directory);
 
-  let project = null;
+  if (!validate(id)) throw new Error('Unable to validate project id');
 
-  if (!validate(id)) throw new Error('Unable to validate id');
-  projectsList.forEach((p) => {
-    if (p._id === id) {
-      return (project = p);
-    }
-  });
-  return project;
+  return listProjects(project_directory).find((p) => p._id === id);
 };
 
 /**
@@ -92,12 +90,11 @@ const getProject = (project_directory, id) => {
  */
 const getProjectFile = (project_directory, id, filePath) => {
   checkFS(project_directory);
-  listProjects(project_directory);
 
   let projectFile = null;
 
   if (!validate(id)) throw new Error('Unable to validate id');
-  projectsList.forEach((p) => {
+  listProjects(project_directory).forEach((p) => {
     if (p._id === id) {
       if (fs.existsSync(`${project_directory}/${p.name}/${filePath}`)) {
         return (projectFile = `/${p.name}/${filePath}`);
@@ -110,7 +107,7 @@ const getProjectFile = (project_directory, id, filePath) => {
 };
 
 /**
- * @description changes project information
+ * @description Changes project information
  * @author NinjaSurge
  * @param {*} project_directory - The directory containing all the projects
  * @param {*} id - The id of the project to change
@@ -121,16 +118,32 @@ const editProject = (project_directory, id, project_info) => {
   // Checks for project folder
   checkFS(project_directory);
 
+  // Find Project
   let project = getProject(project_directory, id);
 
-  // Create the folder structure and config file
+  //Compare and change information
+  let data = getProject(project_directory, id);
+  for (const project_key in project) {
+    // if !=, !undefined, and !_id
+    if (
+      JSON.stringify(project[project_key]) !==
+        JSON.stringify(project_info[project_key]) &&
+      JSON.stringify(project_info[project_key]) !== undefined &&
+      project_key !== '_id'
+    ) {
+      data[project_key] = project_info[project_key];
+    }
+  }
+
+  // Rename
   const dir = `${project_directory}/${project.name}`;
+  const new_dir = `${project_directory}/${project_info.name}`;
   if (!fs.existsSync(dir)) return { error: "Folder or Project Doesn't Exist" };
   try {
-    fs.writeFileSync(`${dir}/.pm.json`, JSON.stringify(project_info), 'utf8');
-    fs.renameSync(dir, `${project_directory}/${project_info.name}`);
+    fs.renameSync(dir, new_dir);
+    fs.writeFileSync(`${new_dir}/.pm.json`, JSON.stringify(data), 'utf8');
   } catch (err) {
-    throw new Error('[pmfs]: ' + err);
+    throw new Error('Error moving files');
   }
   project = getProject(project_directory, id);
   // Return the edited Project
@@ -151,7 +164,7 @@ const makeProject = (project_directory, project_info) => {
   // Handle incoming project information
   const uuid = uuidv4();
   const info = {
-    name: project_info.name,
+    project_info,
     _id: uuid,
   };
 
